6aa6ba3b0440bf1c4f5b1e14f60b3cd9c4a52601
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-03-17 17:30:36 +0100

    start inputFlashCard

--- src/components/inputFlashcard.tsx
+++ src/components/inputFlashcard.tsx
@@ -8,21 +8,35 @@ type Data = {
 type InputFlashcardProps = {
   data: Data;
 };
+type InputFlashcardEvent = {
+  target: {
+    value: string;
+  };
+};
 const InputFlashcard: React.FC<InputFlashcardProps> = (props) => {
+  const [formText, setFormText] = React.useState("");
+  const [wrong, setwrong] = React.useState("");
+  const isItTrue = (text: string) => {};
+  const handleInputText = (event: InputFlashcardEvent) => {
+    console.log(typeof event);
+    const value = event.target.value;
+    setFormText(event.target.value);
+    value ? setwrong("Wrong answer :(") : setwrong("You got it!!");
+  };
   return (
     <div>
       <h1>inputFlashcard</h1>
       <form>
         <div className="form-group">
-          <label htmlFor="firstName">
-            Which operator is used to assign a value to a variable ?
-          </label>
+          <label htmlFor="question">{props.data[1].question}</label>
           <hr />
           <input
+            onChange={handleInputText}
             type="text"
             className="form-control"
-            id="firstName"
+            id="question"
             placeholder="your answer"
+            value={formText}
           />
         </div>
         <p></p>
